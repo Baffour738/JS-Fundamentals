3982e415ce7ae74151bd5ba29ae83f86ba1f1498
Akowuah Addo Baffour <akowuahab@gmail.com>
2025-07-13 23:13:44 +0000

    Update 8-square.js

--- 8-square.js
+++ 8-square.js
@@ -1,9 +1,9 @@
 const arg = process.argv[2];
 const size = parseInt(arg);
-if (isNaN(size) || size <= 0) {
+if (isNaN(size)) {
   console.log('Missing size');
 } 
-else {
+else if (size > 0) {
   for (let i = 0; i < size; i++) {
     console.log('x'.repeat(size));
   }
