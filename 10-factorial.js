d8e3e5a3dab839431c7d2febd24870e4a0eecea1
Akowuah Addo Baffour <akowuahab@gmail.com>
2025-07-13 23:12:26 +0000

    Update 10-factorial.js

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,11 +1,12 @@
 function factorial(n) {
-  if (isNaN(n) || n < 0) {
-    return 1;
-  }
   if (n === 0) {
     return 1;
   }
   return n * factorial(n - 1);
-}        
+}
 const arg = parseInt(process.argv[2]);
-console.log(factorial(arg));
+if (isNaN(arg)) {
+  console.log(1);
+} else {
+  console.log(factorial(arg));
+}
